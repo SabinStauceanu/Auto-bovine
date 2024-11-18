b04fbf88d2b4acca00250f4ceb8a58561fff413d
SabinStauceanu <51922424+SabinStauceanu@users.noreply.github.com>
2024-11-18 16:28:34 +0200

    commit 7

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from pywinauto import application
 from pywinauto.application import Application
 import time
@@ -7,47 +6,66 @@ import xlwings as xw
 from xlwings import Range, constants
 import pydirectinput
 from datetime import date
-import sys
-import ctypes
 import win32api, win32con
+
+caleExcel = "C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls"
+calePutty = "C:\\vifout\\Putty\\putty.exe"
+foaieCalculReceptii = 'Foaie1'
+foaieCalculAutomat = 'Date'
+
+# Functie deschidere consola putty
+
+def deschidereConsola(postLucru):
+    global app
+    app = Application(backend="uia").start(calePutty)
+    pid = application.process_from_module(module=calePutty)
+    # app.PuTTYConfiguration.print_control_identifiers()
+    btnOpen = app.PuTTYConfiguration.child_window(title="Open", auto_id="1009", control_type="Button")
+    srvOption = app.PuTTYConfiguration.child_window(title="srvvif57", control_type="ListItem")
+    srvOption.select()
+    btnOpen.click()
+    time.sleep(1)
+    app = Application(backend="uia").connect(process=pid)
+    pyautogui.typewrite(postLucru)
+    pyautogui.press("enter")
+    time.sleep(1)
+    pyautogui.press("enter", presses=4)
 
 pyautogui.FAILSAFE = False
 pydirectinput.FAILSAFE = False
 
 # Selectare sheet Foaie1 inainte de rularea programului
-xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Foaie1'].select()
+try:
+    xw.Book(caleExcel).sheets[foaieCalculReceptii].select()
+except:
+    ctypes.windll.user32.MessageBoxW(0, "Te rog selecteaza sheet-ul Foaie1", "Eroare selectie sheet!", 0)
+    sys.exit()
 
 #Extragere date din excelul de bovine
 
-wb = xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Foaie1']
-
-
+wb = xw.Book(caleExcel).sheets[foaieCalculReceptii]
 today = date.today()
 formatted_date = today.strftime('%m.%d.%Y')
-if xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("I9").value != formatted_date:
-    #xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("F7").value = "NU"
-    #xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("F9").value = "NU"
-    xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("I9").value = formatted_date
-    xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("G3").value = ""
-
-
+if xw.Book(caleExcel).sheets[foaieCalculAutomat].range("I9").value != formatted_date:
+    xw.Book(caleExcel).sheets[foaieCalculAutomat].range("I9").value = formatted_date
+    xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value = ""
 
 lastCell = wb.range('E' + str(wb.cells.last_cell.row)).end('up').row
-
 nrReceptie = 0
-if xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("G3").value is None:
+
+if xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value is None:
     nrReceptie = 9
 else:
-    nrReceptie = int(xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("G3").value)
+    nrReceptie = int(xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value)
     nrReceptie = nrReceptie + 8
 
-#print(xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("G3").value)
 if nrReceptie == lastCell:
+    verificareCrotal = wb.range("E9" + ":E" + str(lastCell)).value
     nrCrotal = wb.range("E" + str(nrReceptie)).value
     # Se verifica daca celulele sunt goale
     if wb.range("C" + str(nrReceptie)).value is None:
         wb.range("C" + str(nrReceptie)).color = (235, 52, 52)
-        ctypes.windll.user32.MessageBoxW(0, "Lipseste numarul de criteriu:" + str(nrReceptie - 8),"Nr criteriu lipsa!", 0)
+        ctypes.windll.user32.MessageBoxW(0, "Lipseste numarul de criteriu:" + str(nrReceptie - 7),"Nr criteriu lipsa!", 0)
         sys.exit()
     else:
         nrCriteriu = wb.range("C" + str(nrReceptie)).value
@@ -104,6 +122,7 @@ if nrReceptie == lastCell:
 else:
     nrCriteriu = wb.range("C" + str(nrReceptie) + ":C" + str(lastCell)).value
     nrCrotal = wb.range("E" + str(nrReceptie) + ":E" + str(lastCell)).value
+    verificareCrotal = wb.range("E9" + ":E" + str(lastCell)).value
     varsta = wb.range("G" + str(nrReceptie) + ":G" + str(lastCell)).value
     sex = wb.range("H" + str(nrReceptie) + ":H" + str(lastCell)).value
     rasa = wb.range("I" + str(nrReceptie) + ":I" + str(lastCell)).value
@@ -161,14 +180,12 @@ else:
     nrCriteriu = [int(nrCriteriu) for nrCriteriu in nrCriteriu]
     varsta = [int(varsta) for varsta in varsta]
     rasa = [rasa.strip(' ') for rasa in rasa]
-doctor = int(xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("E2").value)
+doctor = int(xw.Book(caleExcel).sheets[foaieCalculAutomat].range("E2").value)
 
 # Verificare rasa inainte de lansare program
 
 listRasa = ["AB ANGUS", "AYRS", "BIVOL", "BU", "BB", "BMM", "BN", "BNR", "BR", "BRAUN", "BRUNA", "CHAROL", "FLECK", "FRIZA", "HER", "HOLL", "JER", "LYM", "MET", "MONTB", "PINZG", "RED HOLL", "RED HOOL", "SIMENT", "SURA", "AUBRAC", "HG"
                                                                                                                                                                                                                              ""]
-
-
 contineRasa = False
 
 if nrReceptie == lastCell:
@@ -177,8 +194,8 @@ if nrReceptie == lastCell:
             contineRasa = True
             break
     if contineRasa == False:
-        wb.range("I" + str(nrReceptie + i)).color = (235, 52, 52)
-        ctypes.windll.user32.MessageBoxW(0, "Reintrodu rasa de la pozitia:" + str(i + nrReceptie - 8), "Rasa incorecta", 0)
+        wb.range("I" + str(nrReceptie)).color = (235, 52, 52)
+        ctypes.windll.user32.MessageBoxW(0, "Reintrodu rasa de la pozitia:" + str(nrReceptie - 8), "Rasa incorecta", 0)
         sys.exit()
 else:
     for i in range(len(rasa)):
@@ -193,6 +210,36 @@ else:
             sys.exit()
         contineRasa = False
 
+# Verificare sex animal
+
+if nrReceptie == lastCell:
+    if sex == "F" or sex == "M":
+       print("")
+    else:
+        wb.range("H" + str(nrReceptie)).color = (235, 52, 52)
+        ctypes.windll.user32.MessageBoxW(0, "Reintrodu sexul de la pozitia:" + str(nrReceptie - 8), "Sex incorect", 0)
+        sys.exit()
+else:
+    for i in range(len(rasa)):
+        if sex[i] == "F" or sex[i] == "M":
+            continue
+        else:
+            wb.range("H" + str(nrReceptie + i)).color = (235, 52, 52)
+            ctypes.windll.user32.MessageBoxW(0, "Reintrodu sexul de la pozitia:" + str(i + nrReceptie - 8), "Sex incorect", 0)
+            sys.exit()
+
+# Verificare crotale duplicate
+
+for i in range(lastCell - 8):
+    for j in range(lastCell - 8):
+
+        if i == j:
+            pass
+        elif verificareCrotal[i] == verificareCrotal[j]:
+            ctypes.windll.user32.MessageBoxW(0, "Crotalul " + verificareCrotal[i] + " este duplicat la pozitia " + str(i+1) + " si pozitia " + str(j+1),
+                                             "Crotal duplicat", 0)
+            sys.exit()
+
 # Se selecteaza o celula goala pentru a evita o eroare
 wb.range("E1").select()
 
@@ -203,22 +250,15 @@ caps_status = win32api.GetKeyState(win32con.VK_CAPITAL)
 if caps_status==1:
     pyautogui.press("capslock")
 
+# Creare lista de crotale pentru furnizori diferiti
+
+listaCrotale= [
+
+]
+
 #Introducere date in p01
 
-app = Application(backend="uia").start("C:\\vifout\\Putty\\putty.exe")
-pid = application.process_from_module(module = "C:\\vifout\\Putty\\putty.exe")
-#app.PuTTYConfiguration.print_control_identifiers()
-btnOpen = app.PuTTYConfiguration.child_window(title="Open", auto_id="1009", control_type="Button")
-srvOption = app.PuTTYConfiguration.child_window(title="srvvif57", control_type="ListItem")
-srvOption.select()
-btnOpen.click()
-time.sleep(1)
-app = Application(backend="uia").connect(process=pid)
-pyautogui.typewrite("p01")
-pyautogui.press("enter")
-time.sleep(1)
-#app.VIF5_7.print_control_identifiers()
-pyautogui.press("enter", presses=4)
+deschidereConsola("p01")
 pyautogui.press('r')
 pyautogui.press('e')
 pyautogui.press('enter', presses=2)
@@ -245,7 +285,7 @@ if nrReceptie == lastCell:
     pyautogui.typewrite(nrCrotal)
     pyautogui.press("enter")
     pyautogui.press("enter")
-    pyautogui.typewrite(nrCrotal[:2])
+    pyautogui.typewrite("UE")
     pyautogui.press("enter")
     pyautogui.typewrite(nrPasaport)
     pyautogui.press("enter")
@@ -273,6 +313,9 @@ if nrReceptie == lastCell:
     time.sleep(1)
     pyautogui.press("f4")
     pyautogui.press("d")
+
+    # Salvare nr criteriu in sheet-ul de date in cazul in care conexiunea la server este intrerupta
+    xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value = nrCriteriu + 1
 else:
     # In cazul in care exita mai multe receptii avem scrptul asta
     pyautogui.press("f3")
@@ -294,7 +337,7 @@ else:
     pyautogui.press("enter")
     pyautogui.press("enter")
 
-    pyautogui.typewrite(nrCrotal[0][:2])
+    pyautogui.typewrite("UE")
     pyautogui.press("enter")
 
     pyautogui.typewrite(nrPasaport[0])
@@ -328,39 +371,79 @@ else:
     pyautogui.press("enter")
     pyautogui.press("f2")
     pyautogui.press("f2")
-    time.sleep(1)
+    time.sleep(3)
+
+    listaCrotale.append(nrCrotal[0])
 
     for i in range(1,len(propietar)):
         if propietarAnterior != propietar[i]:
             pyautogui.press("f4")
             pyautogui.press("d")
+            time.sleep(2)
+            app.VIF5_7.child_window(title="Închidere", control_type="Button").click()
+            pyautogui.press("enter")
+
+            #Salvare nr criteriu in sheet-ul de date in cazul in care conexiunea la server este intrerupta
+            xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value = nrCriteriu[i] + 1
+
+            #Deschidere post 2
+
+            pydirectinput.PAUSE = 0.03
+
+            crotaleSortate = sorted(listaCrotale)
+
+            deschidereConsola("p02")
+            # pp.VIF5_7.print_control_identifiers()
+            pyautogui.press('b')
+            pyautogui.press('e')
             time.sleep(1)
+
+            for j in range(len(listaCrotale)):
+                pyautogui.hotkey('ctrl', 'o')
+                pyautogui.typewrite("10301")
+                pyautogui.press("enter")
+                pyautogui.press("f2")
+                pyautogui.press("enter")
+                pyautogui.typewrite(listaCrotale[j])
+                pydirectinput.press("enter")
+                pydirectinput.press("f2")
+                time.sleep(2.5)
+
+            listaCrotale.clear()
+
+            app.VIF5_7.child_window(title="Închidere", control_type="Button").click()
+            pyautogui.press("enter")
+
+            deschidereConsola("p01")
+            # app.VIF5_7.print_control_identifiers()
+            pyautogui.press('r')
+            pyautogui.press('e')
+            pyautogui.press('enter', presses=2)
+
             pyautogui.press("f3")
-            time.sleep(1)
             pyautogui.press("enter")
             pyautogui.typewrite("0")
             pyautogui.press("enter")
-            time.sleep(1)
+
             pyautogui.typewrite(masina[i])
             pyautogui.press("enter")
+
             pyautogui.typewrite(str(doctor))
             pyautogui.press("enter")
             pyautogui.press("f2")
-            time.sleep(1)
             pyautogui.typewrite("10301")
             pyautogui.press("enter")
 
             pyautogui.typewrite(nrCrotal[i])
             pyautogui.press("enter")
-            time.sleep(1)
             pyautogui.press("enter")
 
-            pyautogui.typewrite(nrCrotal[i][:2])
+            pyautogui.typewrite("UE")
             pyautogui.press("enter")
 
             pyautogui.typewrite(nrPasaport[i])
             pyautogui.press("enter")
-            if rasa[i]== "RED HOOL":
+            if rasa[i] == "RED HOOL":
                 rasa[i] = "RED HOLL"
 
             pyautogui.typewrite(rasa[i])
@@ -389,8 +472,10 @@ else:
             pyautogui.press("enter")
             pyautogui.press("f2")
             pyautogui.press("f2")
-            time.sleep(1)
+            time.sleep(3)
             propietarAnterior = propietar[i]
+            listaCrotale.append(nrCrotal[i])
+
         else:
             pyautogui.press("enter")
 
@@ -399,12 +484,12 @@ else:
             pyautogui.press("enter")
             pyautogui.press("enter")
 
-            pyautogui.typewrite(nrCrotal[i][:2])
+            pyautogui.typewrite("UE")
             pyautogui.press("enter")
 
             pyautogui.typewrite(nrPasaport[i])
             pyautogui.press("enter")
-            if rasa[i]== "RED HOOL":
+            if rasa[i] == "RED HOOL":
                 rasa[i] = "RED HOLL"
 
             pyautogui.typewrite(rasa[i])
@@ -425,21 +510,19 @@ else:
             pyautogui.press("enter")
             pyautogui.press("f2")
             pyautogui.press("f2")
-            time.sleep(1)
+            time.sleep(3)
             propietarAnterior = propietar[i]
+            listaCrotale.append(nrCrotal[i])
     pyautogui.press("f4")
     pyautogui.press("d")
-    time.sleep(1)
-
+    time.sleep(3)
 
 #Memorare in excel urmatoarea introducere
-xw.Book("C:\\Users\\CALITATE\\Desktop\\BOVINA PUTTY.xls").sheets['Date'].range("G3").value = lastCell - 7
+xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value = lastCell - 7
 
 # Inchidere post P01
 app.VIF5_7.child_window(title="Închidere", control_type="Button").click()
 pyautogui.press("enter")
-
-
 
 #Validare date in p02
 
@@ -447,53 +530,40 @@ pydirectinput.PAUSE = 0.02
 
 crotaleSortate = sorted(nrCrotal)
 
-app = Application(backend="uia").start("C:\\vifout\\Putty\\putty.exe")
-pid = application.process_from_module(module = "C:\\vifout\\Putty\\putty.exe")
-#app.PuTTYConfiguration.print_control_identifiers()
-btnOpen = app.PuTTYConfiguration.child_window(title="Open", auto_id="1009", control_type="Button")
-srvOption = app.PuTTYConfiguration.child_window(title="srvvif57", control_type="ListItem")
-srvOption.select()
-btnOpen.click()
-time.sleep(1)
-app = Application(backend="uia").connect(process=pid)
-#pp.VIF5_7.print_control_identifiers()
-pyautogui.typewrite("p02")
-pyautogui.press("enter")
-time.sleep(1)
-pyautogui.press("enter", presses=4)
+deschidereConsola("p02")
+# pp.VIF5_7.print_control_identifiers()
 pyautogui.press('b')
 pyautogui.press('e')
 time.sleep(1)
+
 if nrReceptie == lastCell:
     pyautogui.hotkey('ctrl', 'o')
     pyautogui.typewrite("10301")
     pyautogui.press("enter")
     pyautogui.press("f2")
     pyautogui.press("enter")
-    pyautogui.press("f5")
-    time.sleep(1)
+    pyautogui.typewrite(nrCrotal)
     pydirectinput.press("enter")
     pydirectinput.press("f2")
 else:
-    for i in range(len(propietar)):
+    for i in range(len(listaCrotale)):
         pyautogui.hotkey('ctrl', 'o')
         pyautogui.typewrite("10301")
         pyautogui.press("enter")
         pyautogui.press("f2")
         pyautogui.press("enter")
-        pyautogui.press("f5")
-        time.sleep(2)
-        for j in range(len(nrCrotal)):
-            if nrCrotal[i] != crotaleSortate[j]:
-                pydirectinput.press("down")
-            else:
-                pydirectinput.press("enter")
-                crotaleSortate.pop(j)
-                pydirectinput.press("f2")
-                time.sleep(1.5)
-                break
+        pyautogui.typewrite(listaCrotale[i])
+        pydirectinput.press("enter")
+        pydirectinput.press("f2")
+        time.sleep(2.5)
 
 # Inchidere post P02
 app.VIF5_7.child_window(title="Închidere", control_type="Button").click()
 pyautogui.press("enter")
 
+#Memorare in excel urmatoarea introducere (asta este pentru cand se face doar P02)
+xw.Book(caleExcel).sheets[foaieCalculAutomat].range("G3").value = lastCell - 7
+
+# Salvare fisier excel
+
+xw.Book(caleExcel).save()
